c3ec0a1070dd1201cb8c1d3d0c7925c580ad2bd8
damaroja <ildefonsoarrecife3@hotmail.com>
2023-10-28 19:45:00 +0100

    Update ejemplo.sol

--- solidity/ERC20/ejemplo.sol
+++ solidity/ERC20/ejemplo.sol
@@ -1,1 +1,15 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+
+import "./p1.sol";
+
+
+contract Palomitas is ERC20{
+    constructor() ERC20("Palomitas", "PL"){}
+
+    function createTokens() public {
+        mint(msg.sender, 1000);
+    }
+}
