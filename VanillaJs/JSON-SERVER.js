445e9b8125ad97d3eeb05fdb353a892e87951f95
damaroja <ildefonsoarrecife3@hotmail.com>
2023-12-08 20:31:31 +0000

    Update JSON-SERVER.js

--- VanillaJs/JSON-SERVER.js
+++ VanillaJs/JSON-SERVER.js
@@ -16,3 +16,28 @@ function saveItemJsonServer(objeto) {
         .then( data => console.log(data))
         .catch( error => console.log(error));
 }
+
+Esto lista los objetos de la API de JSON SERVER
+
+async function listarClientes() {
+    const url = 'http://localhost:3000/clientes';
+    await fetch(url)
+        .then( response => response.json())
+        .then( data => {
+            clientesList.innerHTML = '';
+            data.forEach( cliente => {
+                const tr = document.createElement('tr');
+                tr.innerHTML = `
+                    <td>${cliente.nombre}</td>
+                    <td>${cliente.telefono}</td>
+                    <td>${cliente.email}</td>
+                    <td>${cliente.empresa}</td>
+                    <td>
+                        <button class="btn btn-danger" onclick="eliminarCliente(${cliente.id})">Eliminar</button>
+                        <button class="btn btn-warning" onclick="editarCliente(${cliente.id})">Editar</button>
+                    </td>
+                `;
+                clientesList.appendChild(tr);
+            });
+        })
+}
