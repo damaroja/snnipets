c3d05aaf323bc388c46f13f3648dd4c58cc4faa2
damaroja <ildefonsoarrecife3@hotmail.com>
2024-01-12 11:01:36 +0000

    Create Mongo-url-connexion.js

---
+++ node/Mongo-url-connexion.js
@@ -0,0 +1,8 @@
+
+
+
+MONGO_URI=mongodb+srv://ildefonsoarevalo2:uFNeGMBZ6sIhx3ym@cluster0.ugsffdl.mongodb.net/apv?retryWrites=true&w=majority
+                                                                                        III         
+
+
+Donde estan las ies, poner el nombre de la base de datos
