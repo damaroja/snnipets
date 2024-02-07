92b1d44fb5a15b6a640cb4692c4b6cdbc603a867
damaroja <ildefonsoarrecife3@hotmail.com>
2024-02-07 19:59:21 +0000

    Create UseNavigate.js

---
+++ react/UseNavigate.js
@@ -0,0 +1,15 @@
+//Uso del Hook UseNavigate
+
+import { useNavigate } from "react-router-dom";
+
+function useLogoutTimer() {
+  const userIsInactive = useFakeInactiveUser();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (userIsInactive) {
+      fake.logout();
+      navigate("/session-timed-out");
+    }
+  }, [userIsInactive]);
+}
