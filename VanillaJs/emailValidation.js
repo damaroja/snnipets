2d467c9aa73f2a623ea000850e2fcd2a982e9bd0
damaroja <ildefonsoarrecife3@hotmail.com>
2024-02-19 17:29:52 +0000

    Delete VanillaJs/emailValidation.js

--- VanillaJs/emailValidation.js
+++
@@ -1,7 +0,0 @@
-function validarEmail(email) {
-  // Expresión regular para validar direcciones de correo electrónico
-  var expresionRegular = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-
-  // Usamos el método test() de la expresión regular para verificar si el email cumple con el patrón
-  return expresionRegular.test(email);
-}
